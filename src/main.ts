e415b6f5fe3b03536fe12a5fed79a26beda5c84d
moajo <mimirosiasd@gmail.com>
2017-06-04 23:15:44 +0900

    fix: modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,51 +17,52 @@ import GLExtRequestor from "grimoirejs-fundamental/ref/Resource/GLExtRequestor";
 import MaterialFactory from "grimoirejs-fundamental/ref/Material/MaterialFactory";
 import LightVariableRegister from "./Util/LightVariableRegister";
 import SceneComponent from "grimoirejs-fundamental/ref/Components/SceneComponent";
+
 export default () => {
   GrimoireInterface.register(async () => {
-    SceneComponent.onSceneDescriptionCreation((s:LightInfoSceneDesc)=>{
+    SceneComponent.onSceneDescriptionCreation((s: LightInfoSceneDesc) => {
       s.lights = {
         directional: {
           indicies: [] as string[],
-          directions: new VectorArrayContainer(3,0),
-          colors: new VectorArrayContainer(3,0),
-          params: new VectorArrayContainer(4,0)
+          directions: new VectorArrayContainer(3, 0),
+          colors: new VectorArrayContainer(3, 0),
+          params: new VectorArrayContainer(4, 0)
         },
         point: {
           indicies: [] as string[],
-          positions: new VectorArrayContainer(3,0),
-          colors: new VectorArrayContainer(3,0),
-          params: new VectorArrayContainer(2,0)
+          positions: new VectorArrayContainer(3, 0),
+          colors: new VectorArrayContainer(3, 0),
+          params: new VectorArrayContainer(2, 0)
         },
         spot: {
           indicies: [] as string[],
-          positions: new VectorArrayContainer(3,0),
-          directions: new VectorArrayContainer(3,0),
-          colors: new VectorArrayContainer(3,0),
-          params: new VectorArrayContainer(4,0)
+          positions: new VectorArrayContainer(3, 0),
+          directions: new VectorArrayContainer(3, 0),
+          colors: new VectorArrayContainer(3, 0),
+          params: new VectorArrayContainer(4, 0)
         },
-        shadowMap:{
-          shadowMapCountPerEdge:new Vector2(0,0),
-          shadowMap:null,
-          lightMatrices:null,
-          pixelSize:0,
-          count:0
+        shadowMap: {
+          shadowMapCountPerEdge: new Vector2(0, 0),
+          shadowMap: null,
+          lightMatrices: null,
+          pixelSize: 0,
+          count: 0
         }
       };
     });
     const g = GrimoireInterface;
-    g.registerComponent("ForwardShadingManager",ForwardShadingManager);
-    g.registerComponent("Light",LightComponent);
-    g.registerComponent("DirectionalLightType",DirectionalLightTypeComponent);
-    g.registerComponent("PointLightType",PointLightTypeComponent);
-    g.registerComponent("SpotLightType",SpotLightTypeComponent);
-    g.registerComponent("SceneLightManager",SceneLightManager);
-    g.registerComponent("ShadowMapCamera",ShadowMapCameraComponent);
-    g.registerComponent("RenderShadowMap",RenderShadowMapComponent);
-    g.registerComponent("SpotLightShadowMapCamera",SpotLightShadowMapCameraComponent);
-    g.overrideDeclaration("scene",["SceneLightManager"]);
-    g.overrideDeclaration("render-scene",["RenderShadowMap"]);
-    g.nodeDeclarations.get("goml").defaultComponents.push(g.ns("http://grimoire.gl/ns/default")("ForwardShadingManager"));
+    g.registerComponent("ForwardShadingManager", ForwardShadingManager);
+    g.registerComponent("Light", LightComponent);
+    g.registerComponent("DirectionalLightType", DirectionalLightTypeComponent);
+    g.registerComponent("PointLightType", PointLightTypeComponent);
+    g.registerComponent("SpotLightType", SpotLightTypeComponent);
+    g.registerComponent("SceneLightManager", SceneLightManager);
+    g.registerComponent("ShadowMapCamera", ShadowMapCameraComponent);
+    g.registerComponent("RenderShadowMap", RenderShadowMapComponent);
+    g.registerComponent("SpotLightShadowMapCamera", SpotLightShadowMapCameraComponent);
+    g.overrideDeclaration("scene", ["SceneLightManager"]);
+    g.overrideDeclaration("render-scene", ["RenderShadowMap"]);
+    g.overrideDeclaration("goml", ["ForwardShadingManager"]);
     g.registerNode("light", ["Transform", "Light"]);
     LightVariableRegister.registerAll();
     GLExtRequestor.request("OES_texture_float");
